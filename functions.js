518518a37b0cbcc5a7ab273e4bc3e97871f75fae
kaceykooGithub <kacey.koo@gmail.com>
2025-06-28 16:26:09 +0900

    バグ修正

--- functions.js
+++ functions.js
@@ -216,7 +216,7 @@ function getSongs() {
  * @returns {string} The string with all katakana characters converted to hiragana characters.
  */
 function kataToHira(str) {
-     return str.replace(/[\u30a1-\u30f6]/g, function (match) {
+     return `${str}`.replace(/[\u30a1-\u30f6]/g, function (match) {
           var chr = match.charCodeAt(0) - 0x60;
           return String.fromCharCode(chr);
      });
